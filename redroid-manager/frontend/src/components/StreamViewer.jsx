ae299c739f45aaee1a0b9dfcb0182dd6fd9f534c
ittiphol <ittiphol.jomg@gmail.com>
2026-04-29 03:27:30 +0700

    Switch to professional Stream Only mode with standard iframe sizing

--- redroid-manager/frontend/src/components/StreamViewer.jsx
+++ redroid-manager/frontend/src/components/StreamViewer.jsx
@@ -27,8 +27,8 @@ export default function StreamViewer({ selectedDevice }) {
       const wsUrl = `ws://${wsHost}:8001/?action=proxy-adb&remote=tcp:8886&udid=${udid}`;
       
       // กลับไปใช้ #! สำหรับ main URL เพราะการใช้ ? ทำให้ ws-scrcpy ค้างที่หน้า Device Tracker
-      // เพิ่มพารามิเตอร์ซ่อน UI ส่วนเกินของ ws-scrcpy ให้ครบถ้วนที่สุดเพื่อให้เหลือแต่หน้าจอ Android
-      const url = `http://${host}:8001/#!action=stream&udid=${encodeURIComponent(udid)}&player=mse&hide-navbar=1&hide-control=1&hide-settings=1&hide-menu=1&hide-header=1&hide-list=1&inline=1&ws=${encodeURIComponent(wsUrl)}`;
+      // ใช้พารามิเตอร์สำหรับโหมด Stream โดยเฉพาะ เพื่อให้ ws-scrcpy แสดงผลเต็มหน้าต่าง iframe
+      const url = `http://${host}:8001/#!action=stream&udid=${encodeURIComponent(udid)}&player=mse&hide-header=1&hide-navbar=1&hide-footer=1&hide-menu=1&hide-control=1&ws=${encodeURIComponent(wsUrl)}`;
       
       return url;
     }
